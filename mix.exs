e3d7d6eace6ce6ccb709d9472c130b5d16bb5913
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-04-05 21:05:41 -0600

    Finish off package config metadata

--- mix.exs
+++ mix.exs
@@ -1,22 +1,28 @@
 defmodule MalanUtils.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/freedomben/malan_utils"
+  @version "0.1.0"
+
   def project do
     [
       app: :malan_utils,
-      name: "malan_utils",
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      app: :postgrex,
-      version: "0.1.0",
-      elixir: "~> 1.0",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-      description: description(),
+      description: "Set of utility functions initially written for the Malan project, but now independent.",
       package: package(),
-      source_url: "https://github.com/FreedomBen/malan_utils"
+      deps: deps(),
+      docs: docs()
+    ]
+  end
+
+  def package do
+    [
+      name: "malan_utils",
+      maintainers: ["Benjmain Porter"],
+      licenses: ["MIT", "Apache-2.0"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 
@@ -30,18 +36,17 @@ defmodule MalanUtils.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:pbkdf2_elixir, "~> 1.2"}
+      {:ex_doc, "~> 0.28.0"},
+      {:pbkdf2_elixir, "~> 1.2"},
     ]
   end
 
-  defp description() do
-    "Set of utility functions initially written for the Malan project, but now independent."
-  end
-
-  defp package() do
+  defp docs do
     [
-      licenses: ["MIT", "Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/FreedomBen/malan_utils"}
+      main: "MalanUtils",
+      source_url: @source_url,
+      extra_section: [],
+      api_reference: false
     ]
   end
 end
