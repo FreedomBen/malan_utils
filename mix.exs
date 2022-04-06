1985d88a620c5b27022f829588c8dccdd33c9e43
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-04-05 20:53:06 -0600

    Add package metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,19 @@ defmodule MalanUtils.MixProject do
   def project do
     [
       app: :malan_utils,
+      name: "malan_utils",
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      app: :postgrex,
+      version: "0.1.0",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/FreedomBen/malan_utils"
     ]
   end
 
@@ -22,6 +31,17 @@ defmodule MalanUtils.MixProject do
   defp deps do
     [
       {:pbkdf2_elixir, "~> 1.2"}
+    ]
+  end
+
+  defp description() do
+    "Set of utility functions initially written for the Malan project, but now independent."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT", "Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/FreedomBen/malan_utils"}
     ]
   end
 end
